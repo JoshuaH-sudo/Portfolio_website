1593228169891ce3d808ca5bdc4cd32f69dceaa3
joshuaH-sudo <JHwork101@protonmail.com>
2024-03-22 11:31:41 +0100

    Refactor page layout and styling for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -2,22 +2,26 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="text-offWhite bg-offBlack flex h-screen flex-col items-center justify-between gap-10 p-24">
-      <div className="flex flex-row gap-5">
+    <main className="flex h-screen flex-col items-center justify-between gap-10 bg-offBlack p-12 text-offWhite sm:p-24">
+      <div className="flex flex-col-reverse gap-5 sm:flex-row items-center">
         <Image
           src="/images/image_8.jpg"
           alt="Joshua Hoban"
           width={200}
           height={200}
-          className="rounded-3xl"
+          className="w-full sm:w-[200px] sm:h-[200px] object-cover rounded-3xl"
         />
-        <section className="flex flex-col gap-1 max-w-3xl">
-          <h4 className="text-sm font-bold text-accent">{`G'day my name is,`}</h4>
-          <h1 className="text-8xl font-bold text-primary">Joshua <span className="text-primaryDark">Hoban</span></h1>
-          <h2 className="text-4xl font-bold text-highlight1">
+        <section className="flex max-w-3xl flex-col gap-1">
+          <h4 className="text-md font-bold text-accent sm:text-sm">
+            G&apos;day my name is,
+          </h4>
+          <h1 className="text-4xl font-bold text-primary sm:text-8xl">
+            Joshua <span className="text-primaryDark">Hoban</span>
+          </h1>
+          <h2 className="text-2xl font-bold text-highlight1 sm:text-2xl">
             Experienced Full-Stack Web Developer
           </h2>
-          <p className="text-highlight2 max-w-xl">
+          <p className="text-highlight2 max-w-xl text-xs sm:text-lg">
             Aussie software developer based in Berlin, excelling in Frontend
             design and client satisfaction, while fostering lively work
             environments and expediting projects through efficient prototyping
