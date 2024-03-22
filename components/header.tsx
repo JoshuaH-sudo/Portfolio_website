ac2519c31018f5c766084073427babc4e3a4bf16
joshuaH-sudo <JHwork101@protonmail.com>
2024-03-22 11:34:54 +0100

    Update header styles for mobile

--- components/header.tsx
+++ components/header.tsx
@@ -9,19 +9,19 @@ export default function Header() {
     "bg-offBlack block h-0.5 transition-all duration-500 group-hover:max-w-full";
 
   return (
-    <header className="relative h-20">
+    <header className="relative h-14 sm:h-20">
       <Link href="/">
-        <h1 className="text-offBlack absolute -bottom-3 left-5 text-7xl font-extrabold italic">
+        <h1 className="absolute -bottom-2 left-1 text-4xl font-extrabold italic text-offBlack sm:-bottom-3 sm:left-5 sm:text-7xl">
           JH
         </h1>
       </Link>
       <nav
         className="
-      text-offWhite flex h-full flex-row items-end justify-center
-      bg-primaryLight pb-0.5 text-lg font-bold
+      flex h-full flex-row items-end justify-center bg-primaryLight
+      pb-0.5 text-xs font-bold text-offWhite sm:text-lg
     "
       >
-        <ul className="text-offBlack flex flex-row justify-center gap-5">
+        <ul className="flex flex-row justify-center gap-5 text-offBlack">
           <li className="group transition duration-300">
             <Link href="/">Home</Link>
             <span
