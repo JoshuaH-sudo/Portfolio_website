309869295818da903e36e6b7ad1b782a2f6dfdb3
joshuaH-sudo <JHwork101@protonmail.com>
2024-03-22 13:59:33 +0100

    Refactor header component and fix color bug

--- components/header.tsx
+++ components/header.tsx
@@ -7,14 +7,12 @@ import { useEffect, useState } from "react";
 import { Config } from "tailwindcss";
 
 export default function Header() {
+  const pathname = usePathname();
+
   //Ignore the type error here as it does not know about the custom properties in the tailwind config
   const twConfig = resolveConfig<Config & any>(tailwindConfig);
   const colors = twConfig.theme.colors;
-  const pathname = usePathname();
   const [headerColor, setHeaderColor] = useState(colors.primaryLight);
-
-  const underlineClass =
-    "bg-offBlack block h-0.5 transition-all duration-500 group-hover:max-w-full";
 
   useEffect(() => {
     switch (pathname) {
@@ -33,25 +31,24 @@ export default function Header() {
     }
   }, [colors.highlight1, colors.primaryLight, colors.secondaryLight, pathname]);
 
+  const underlineClass =
+    "bg-offBlack block h-0.5 transition-all duration-500 group-hover:max-w-full";
+
   return (
     <header
       style={{
         //Need to set the color here as tailwind will not be able to resolve the color after state change
         backgroundColor: headerColor,
       }}
-      className={`fixed top-0 h-14 w-full shadow-xl transition-colors duration-300 sm:h-20`}
+      className="fixed top-0 h-14 w-full shadow-xl transition-colors duration-300 sm:h-20"
     >
       <Link href="/">
         <h1 className="absolute -bottom-0 left-1 text-4xl font-extrabold italic text-offBlack sm:-bottom-0 sm:left-5 sm:text-7xl">
           JH
         </h1>
       </Link>
-      <nav
-        className={`
-      flex h-full flex-row items-end justify-center pb-0.5 text-xs
-      font-bold text-offWhite sm:text-lg
-    `}
-      >
+      
+      <nav className="flex h-full flex-row items-end justify-center pb-0.5 text-xs font-bold text-offWhite sm:text-lg">
         <ul className="flex flex-row justify-center gap-5 text-offBlack">
           <li className="group transition duration-300">
             <Link href="/">Home</Link>
