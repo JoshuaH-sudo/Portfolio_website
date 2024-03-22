f75d5065648db7324d8555f8f297909986c202e9
joshuaH-sudo <JHwork101@protonmail.com>
2024-03-22 20:24:18 +0100

    Update navigation test and remove husky dependency

--- cypress/e2e/navigation.cy.ts
+++ cypress/e2e/navigation.cy.ts
@@ -1,9 +1,12 @@
-describe("template spec", () => {
-  it("passes", () => {
+describe("Navigation", () => {
+  it("should navigate to the correct pages from header", () => {
     cy.visit("/");
     cy.get("h1").should("contain", "Joshua");
     // Click the about link
     cy.get("nav a").contains("About").click();
     cy.get("h1").should("contain", "Welcome to my corner of the web!");
+    // click the project link
+    cy.get("nav a").contains("Projects").click();
+    cy.get("h1").should("contain", "Projects");
   });
 });
