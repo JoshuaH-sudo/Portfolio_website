103c73ad79d059424e4342ecbba9f9f9c8dda09a
joshuaH-sudo <JHwork101@protonmail.com>
2024-03-22 20:24:11 +0100

    Update navigation.cy.ts with new content
    REmove husky library

--- cypress/e2e/navigation.cy.ts
+++ cypress/e2e/navigation.cy.ts
@@ -1,6 +1,9 @@
 describe("template spec", () => {
   it("passes", () => {
     cy.visit("/");
-    cy.get("h1").should("contain", "Joshua Hoban");
+    cy.get("h1").should("contain", "Joshua");
+    // Click the about link
+    cy.get("nav a").contains("About").click();
+    cy.get("h1").should("contain", "Welcome to my corner of the web!");
   });
 });
