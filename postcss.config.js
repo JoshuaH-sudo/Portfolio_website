2a53535dbb9fc02cf57c7b4622a352799d19ffb5
JoshuaH-sudo <JHwork101@protonmail.com>
2025-01-30 20:56:56 +1100

    Remove unused color variables plugin from PostCSS configuration

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,5 @@
 module.exports = {
   plugins: {
     '@tailwindcss/postcss': {},
-    addVariablesForColors
   },
 };
-
-// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
-function addVariablesForColors({ addBase, theme }) {
-  let allColors = flattenColorPalette(theme("colors"));
-  let newVars = Object.fromEntries(
-    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
-  );
- 
-  addBase({
-    ":root": newVars,
-  });
-}
