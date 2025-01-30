6a7fa7834c15fc2606fe85176f28bc51fbc43918
JoshuaH-sudo <JHwork101@protonmail.com>
2025-01-30 20:26:53 +1100

    Refactor Home component layout and update header links for improved navigation

--- app/page.tsx
+++ app/page.tsx
@@ -10,19 +10,48 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <main className="flex h-screen flex-col items-center justify-center">
-      <EvervaultCard>
-        <div className="flex h-screen flex-col items-center justify-center">
-          <h1>Joshua Hoban</h1>
-          <h2>Front-end Specialized Full-stack Developer.</h2>
-          <Image
-            src="/images/profile.png"
-            width={322}
-            height={322}
-            alt="profile photo"
-          />
-        </div>
-      </EvervaultCard>
+    <main>
+      <div
+        id="home"
+        className="flex h-screen flex-col items-center justify-center"
+      >
+        <EvervaultCard>
+          <div className="relative flex h-screen flex-col items-center justify-center">
+            <h1>Joshua Hoban</h1>
+            <h2>Front-end Specialized Full-stack Freelance Developer.</h2>
+            <Image
+              src="/images/profile.png"
+              width={322}
+              height={322}
+              alt="profile photo"
+            />
+          </div>
+        </EvervaultCard>
+      </div>
+      <div
+        id="skills"
+        className="flex h-screen flex-col items-center justify-center"
+      >
+        <h1>Skills</h1>
+      </div>
+      <div
+        id="projects"
+        className="flex h-screen flex-col items-center justify-center"
+      >
+        <h1>Projects</h1>
+      </div>
+      <div
+        id="experience"
+        className="flex h-screen flex-col items-center justify-center"
+      >
+        <h1>Experience</h1>
+      </div>
+      <div
+        id="contact"
+        className="flex h-screen flex-col items-center justify-center"
+      >
+        <h1>Contact</h1>
+      </div>
     </main>
   );
 }
--- components/header.tsx
+++ components/header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className="font-andale-mono fixed top-0 z-10 flex w-full items-center justify-between px-5 py-1 transition-colors duration-300">
-      <Link href="/">
+      <Link href="#home">
         <p className="text-off-black font-monomaniac-one text-xl font-extrabold">
           Joshua Hoban
         </p>
@@ -13,19 +13,19 @@ export default function Header() {
       <nav className="flex flex-row justify-end pr-2 pb-0.5 text-lg font-bold">
         <ul className="text-off-black flex flex-row justify-center gap-5">
           <li className="group transition duration-300">
-            <Link href="/">//Home</Link>
+            <Link href="#home">//Home</Link>
           </li>
           <li className="group transition duration-300">
-            <Link href="/about">//Skills</Link>
+            <Link href="#skills">//Skills</Link>
           </li>
           <li className="group transition duration-300">
-            <Link href="/projects">//Projects</Link>
+            <Link href="#projects">//Projects</Link>
           </li>
           <li className="group transition duration-300">
-            <Link href="/experience">//Experience</Link>
+            <Link href="#experience">//Experience</Link>
           </li>
           <li className="group transition duration-300">
-            <Link href="/contact">//Contact</Link>
+            <Link href="#contact">//Contact</Link>
           </li>
         </ul>
       </nav>
