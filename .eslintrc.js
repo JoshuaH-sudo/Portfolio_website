efe9d19c978a67e9ff07fe79b4f3237f2243f100
JoshuaH-sudo <JHwork101@protonmail.com>
2025-01-30 19:01:34 +1100

    Replace .eslintrc.json with .eslintrc.js and update ESLint configuration for improved linting support

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  root: true,
+  extends: [
+    "next",
+    "next/core-web-vitals",
+    "next/typescript",
+    "prettier"
+  ],
+  plugins: [
+    "@next/eslint-plugin-next"
+  ]
+};
