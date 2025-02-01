3500e6ab85920835b3d693215d56ffeeb140ae86
JoshuaH-sudo <JHwork101@protonmail.com>
2025-02-01 18:47:24 +1100

    Refactor random string generation to create random JavaScript code instead

--- components/ui/EverVault.tsx
+++ components/ui/EverVault.tsx
@@ -17,7 +17,7 @@ export const EverVault = ({
   const [randomString, setRandomString] = useState("");
 
   useEffect(() => {
-    let str = generateRandomString(15000);
+    let str = generateRandomJSCode(15000);
     setRandomString(str);
   }, []);
 
@@ -83,6 +83,111 @@ export const generateRandomString = (length: number) => {
   }
   return result;
 };
+const jsKeywords = [
+  "break", "case", "catch", "class", "const", "continue", "debugger", "default", "delete", "do", "else", "export", "extends", "finally", "for", "function", "if", "import", "in", "instanceof", "new", "return", "super", "switch", "this", "throw", "try", "typeof", "var", "void", "while", "with", "yield"
+];
+
+const jsIdentifiers = [
+  "foo", "bar", "baz", "qux", "quux", "corge", "grault", "garply", "waldo", "fred", "plugh", "xyzzy", "thud"
+];
+
+const jsOperators = ["+", "-", "*", "/", "=", "==", "===", "&&", "||", "!", ">", "<", ">=", "<="];
+
+const symbols = ["(", ")", "{", "}", "[", "]", ";", ","];
+
+const generateRandomJSCode = (length: number) => {
+  let result = "";
+  let depth = 0;
+
+  const getRandomElement = (arr: string[]) => arr[Math.floor(Math.random() * arr.length)];
+
+  for (let i = 0; i < length; i++) {
+    const rand = Math.random();
+    if (rand < 0.05 && depth > 0) {
+      result += "} ";
+      depth--;
+    } else if (rand < 0.1) {
+      result += getRandomElement(symbols) + " ";
+    } else if (rand < 0.15) {
+      result += getRandomElement(jsOperators) + " ";
+    } else if (rand < 0.2) {
+      result += "console.log('" + getRandomElement(jsIdentifiers) + "'); ";
+    } else if (rand < 0.25) {
+      result += getRandomElement(jsIdentifiers) + " ";
+    } else if (rand < 0.3) {
+      result += getRandomElement(jsKeywords) + " ";
+    } else if (rand < 0.35) {
+      result += "function " + getRandomElement(jsIdentifiers) + "() { ";
+      depth++;
+    } else if (rand < 0.4) {
+      result += "if (" + getRandomElement(jsIdentifiers) + " " + getRandomElement(jsOperators) + " " + getRandomElement(jsIdentifiers) + ") { ";
+      depth++;
+    } else if (rand < 0.45) {
+      result += "for (let " + getRandomElement(jsIdentifiers) + " = 0; " + getRandomElement(jsIdentifiers) + " < " + Math.floor(Math.random() * 10) + "; " + getRandomElement(jsIdentifiers) + "++) { ";
+      depth++;
+    } else if (rand < 0.5) {
+      result += "while (" + getRandomElement(jsIdentifiers) + " " + getRandomElement(jsOperators) + " " + getRandomElement(jsIdentifiers) + ") { ";
+      depth++;
+    } else if (rand < 0.55) {
+      result += "switch (" + getRandomElement(jsIdentifiers) + ") { case " + getRandomElement(jsIdentifiers) + ": console.log('" + getRandomElement(jsIdentifiers) + "'); break; ";
+      depth++;
+    } else if (rand < 0.6) {
+      result += "Math." + getRandomElement(["abs", "ceil", "floor", "max", "min", "random", "round", "sqrt"]) + "(" + getRandomElement(jsIdentifiers) + "); ";
+    } else if (rand < 0.65) {
+      result += "Array.prototype." + getRandomElement(["push", "pop", "shift", "unshift", "slice", "splice", "forEach", "map", "filter", "reduce"]) + ".call(" + getRandomElement(jsIdentifiers) + ", " + getRandomElement(jsIdentifiers) + "); ";
+    } else {
+      result += getRandomElement(jsIdentifiers) + " ";
+    }
+  }
+
+  while (depth > 0) {
+    result += "} ";
+    depth--;
+  }
+
+  return result.trim();
+};
+
+// const generateRandomJSCode = (length: number) => {
+//   let result = "";
+//   let depth = 0;
+
+//   const getRandomElement = (arr: string[]) => arr[Math.floor(Math.random() * arr.length)];
+
+//   for (let i = 0; i < length; i++) {
+//     const rand = Math.random();
+//     if (rand < 0.05 && depth > 0) {
+//       result += "} ";
+//       depth--;
+//     } else if (rand < 0.1) {
+//       result += getRandomElement(symbols) + " ";
+//     } else if (rand < 0.15) {
+//       result += getRandomElement(jsOperators) + " ";
+//     } else if (rand < 0.2) {
+//       result += getRandomElement(jsIdentifiers) + " ";
+//     } else if (rand < 0.25) {
+//       result += getRandomElement(jsKeywords) + " ";
+//     } else if (rand < 0.3) {
+//       result += "function " + getRandomElement(jsIdentifiers) + "() { ";
+//       depth++;
+//     } else if (rand < 0.35) {
+//       result += "if (" + getRandomElement(jsIdentifiers) + " " + getRandomElement(jsOperators) + " " + getRandomElement(jsIdentifiers) + ") { ";
+//       depth++;
+//     } else if (rand < 0.4) {
+//       result += "switch (" + getRandomElement(jsIdentifiers) + ") { case " + getRandomElement(jsIdentifiers) + ": break; ";
+//       depth++;
+//     } else {
+//       result += getRandomElement(jsIdentifiers) + " ";
+//     }
+//   }
+
+//   while (depth > 0) {
+//     result += "} ";
+//     depth--;
+//   }
+
+//   return result.trim();
+// };
 
 export const Icon = ({ className, ...rest }: any) => {
   return (
