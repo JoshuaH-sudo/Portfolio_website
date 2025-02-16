5eaec6b4b46be2578b0a96368bcadf123022adc4
JoshuaH-sudo <JHwork101@protonmail.com>
2025-02-16 15:52:19 +1100

    Replace react-cookie-consent with js-cookie for cookie management and update consent handling logic

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,7 +2,7 @@
 import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
 import { PropsWithChildren, useEffect, useState } from "react";
-import CookieConsent, { getCookieConsentValue } from "react-cookie-consent";
+import Cookies from "js-cookie";
 
 if (typeof window !== "undefined") {
   posthog.init("phc_Cneu9QyCvYOGFH5oRyNZXkaC3ws4lo5KxiFgktJzcc9", {
@@ -12,21 +12,40 @@ if (typeof window !== "undefined") {
 
 export function CSPostHogProvider({ children }: PropsWithChildren<{}>) {
   const [hasConsent, setHasConsent] = useState(false);
+  const [showBanner, setShowBanner] = useState(false);
 
-  useEffect(() => {
-    const cookieConsent = getCookieConsentValue();
+  const handleConsent = (consent: boolean) => {
+    Cookies.set("cookieConsent", consent.toString(), { expires: 365 });
+    setHasConsent(consent);
+    setShowBanner(false);
+  };
 
-    if (cookieConsent === "true") {
+  const getCookieConsent = async () => {
+    const consent = Cookies.get("cookieConsent");
+    console.log(consent);
+
+    if (!consent) {
+      setShowBanner(true);
+      return setHasConsent(false);
+    }
+
+    if (consent === "true") {
       setHasConsent(true);
-    } else if (cookieConsent === "false") {
+    } else if (consent === "false") {
       setHasConsent(false);
     }
+  };
+
+  useEffect(() => {
+    getCookieConsent();
   }, []);
 
   useEffect(() => {
     if (hasConsent && window.location.hostname !== "localhost") {
+      console.debug("Opting in to capturing");
       posthog.opt_in_capturing();
     } else {
+      console.debug("Opting out of capturing");
       posthog.opt_out_capturing();
     }
   }, [hasConsent]);
@@ -34,35 +53,27 @@ export function CSPostHogProvider({ children }: PropsWithChildren<{}>) {
   return (
     <>
       <PostHogProvider client={posthog}>{children}</PostHogProvider>
-      <CookieConsent
-        style={{
-          background: "#f4f0f0",
-          border: "1px solid #2d2828",
-        }}
-        buttonStyle={{
-          background: "f4f0f0",
-          color: "#2d2828",
-          borderRadius: "0.25rem",
-          border: "1px solid #2d2828",
-        }}
-        buttonClasses="hover:scale-105 transition-transform shadow-md bg-off-white"
-        buttonText="Accept"
-        onAccept={() => setHasConsent(true)}
-        enableDeclineButton
-        declineButtonStyle={{
-          background: "#f4f0f0",
-          color: "#2d2828",
-          borderRadius: "0.25rem",
-          border: "1px solid #2d2828",
-        }}
-        declineButtonClasses="hover:scale-105 transition-transform shadow-md"
-        declineButtonText="Decline"
-        onDecline={() => setHasConsent(false)}
-      >
-        <p className="text-off-black">
-          This website uses cookies to enhance the user experience.
-        </p>
-      </CookieConsent>
+      {showBanner && (
+        <div className="bg-off-black dark:bg-off-white fixed right-0 bottom-30 left-0 z-100 flex flex-col items-center justify-between gap-2 py-2 md:py-5 px-20 md:flex-row">
+          <p className="text-off-black">
+            This website uses cookies to enhance the user experience.
+          </p>
+          <div className="flex gap-2 self-end md:self-center">
+            <button
+              onClick={() => handleConsent(true)}
+              className="border-off-black text-off-white bg-off-black mr-2 rounded-md border-2 px-3 py-1"
+            >
+              Accept
+            </button>
+            <button
+              onClick={() => handleConsent(false)}
+              className="border-off-black text-off-black rounded-md border-2 px-3 py-1"
+            >
+              Decline
+            </button>
+          </div>
+        </div>
+      )}
     </>
   );
 }
