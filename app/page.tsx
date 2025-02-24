3705c429b0a6d075f7a40c3e8842e571ad4e304e
JoshuaH-sudo <JHwork101@protonmail.com>
2025-02-24 16:45:10 +1100

    Refactor project and home pages; consolidate experience types and enhance layout with new BreathingGrid component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import ContactPage from "@/components/pages/contact/contact-page";
-import ExperiencePage from "@/components/pages/experience/experience-page";
-import ExpertisePage from "@/components/pages/expertise/expertise-page";
-import { HomePage } from "@/components/pages/home/home-page";
-import ProjectsPage from "@/components/pages/projects/projects-page";
+import ContactPage from "@/components/pages/5-contact/contact-page";
+import ExperiencePage from "@/components/pages/4-experience/experience-page";
+import ExpertisePage from "@/components/pages/2-expertise/expertise-page";
+import { HomePage } from "@/components/pages/1-home/home-page";
+import ProjectsPage from "@/components/pages/3-projects/projects-page";
 import { TracingBeam } from "@/components/ui/tracing-beam";
 
 export default function Home() {
