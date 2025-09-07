bdc54245e4df2f3a802a7c9dacfd917332704e13
JoshuaH-sudo <Joshua_Hoban@proton.me>
2025-09-07 13:08:56 +0200

    style: add google site verification meta tag for SEO

--- app/_app.tsx
+++ app/_app.tsx
@@ -7,6 +7,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="robots" content="all" />
+        <meta name="google-site-verification" content="IkPDk7mPq-mEFT0ZF-l4GSKRZDTC0xPa-qMfAZjZChU" />
       </Head>
       <Component {...pageProps} />
     </>
